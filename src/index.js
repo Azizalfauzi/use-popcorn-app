60f16fb6974ab7eff66cc7bc94d2d6835e6a92eb
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-08 09:32:33 +0700

    Adding message in StarRating component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      defaultRating={3}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
   </React.StrictMode>
 );
