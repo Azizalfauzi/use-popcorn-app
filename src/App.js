df9745d7c31971d344dc0f56cd571b8a9589a418
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-17 14:16:19 +0700

    Handle search querywith abort function

--- src/App.js
+++ src/App.js
@@ -97,12 +97,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok)
             throw new Error("Something went wrong witj fetching movies");
@@ -123,6 +126,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
