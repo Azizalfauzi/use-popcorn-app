9e41c005c1879c25a7408d2a97aaaa3bd2ac652e
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-22 13:40:25 +0700

    handle data empty after reload with localstorage

--- src/App.js
+++ src/App.js
@@ -57,11 +57,15 @@ const KEY = "cefb801b";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const tempQuery = "interstellar";
   const [selectedId, setSelectedId] = useState("");
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   // useEffect(function () {
   //   console.log("After initial render");
@@ -89,11 +93,20 @@ export default function App() {
 
   function handleAddWatch(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatch(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
