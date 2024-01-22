a6191bb4900c45cdac3c6e2e7cf3999996b28366
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-23 05:37:14 +0700

    Handle rating star bug and fix it

--- src/App.js
+++ src/App.js
@@ -202,6 +202,11 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+  useEffect(function () {
+    if (userRating) countRef.current = countRef.current + 1;
+  }, userRating);
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
 
   const watchedUserRating = watched.find(
@@ -230,6 +235,7 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchMovie);
     onCloseMovie();
